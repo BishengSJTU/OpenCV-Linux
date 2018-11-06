4de35011a821dd4decd8a8a4cda32a2d984bcbb3
ShengBi <39233852+BishengSJTU@users.noreply.github.com>
2018-11-06 19:19:24 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "iostream"
+#include<opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+    Mat img = imread("haha.jpg");
+    if (img.empty()) {
+        cout << "Error" << endl;
+        return -1;
+
+    }
+    imshow("Lena", img);
+    waitKey();
+    return 0;
+}
